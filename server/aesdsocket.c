6dc11789c661828488a429eb7d3901b9eab513e2
SagaXSaga <f74106238@gs.ncku.edu.tw>
2023-05-29 14:55:55 +0800

    HW5 part1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,40 +1,30 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <signal.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <syslog.h>
-#include <errno.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
+#include <syslog.h>
+#include <unistd.h>
 
-#define USER_PORT 9000
-#define READ_SIZE 4096
+#define BUFFER_SIZE 100
 #define DATA_PATH "/var/tmp/aesdsocketdata"
 
-int socket_fd;
+int32_t socket_fd;
 
-void signal_handler(int sig) {
+static void signal_handler(int signal_number) {
   syslog(LOG_INFO, "Caught signal, exiting");
-  close(sockfd);
-  close(client_sockfd);
-  close(socket_file_fd);
   remove(DATA_PATH);
-  
-  if (buffer_ptr) {
-        free(buffer_ptr);
-  }
-  exit(0);
+  shutdown(socket_fd, SHUT_RDWR);
 }
 
 int main(int argc, char *argv[]) {
-  int opt = 0;
-  int sockflgs = 1;
-  struct addrinfo myinfoin = {.ai_flags = AI_PASSIVE,
+  int32_t opt = 0;
+  int32_t sockflgs = 1;
+  struct addrinfo myaddrinfoin = {.ai_flags = AI_PASSIVE,
                                   .ai_family = PF_INET,
                                   .ai_socktype = SOCK_STREAM,
                                   .ai_protocol = 0,
@@ -42,49 +32,47 @@ int main(int argc, char *argv[]) {
                                   .ai_addr = NULL,
                                   .ai_canonname = NULL,
                                   .ai_next = NULL};
-  struct addrinfo *myinfo;
+  struct addrinfo *myaddrinfo;
 
-  openlog(NULL, 0, LOG_USER);// Set up syslog
+  openlog(NULL, 0, LOG_USER);
 
-  // Set up signal handling for SIGTERM and SIGINT
-  struct sigaction s_action;
-  memset(&s_action, 0, sizeof(s_action));
-  s_action.sa_handler = signal_handler;
-  if (sigaction(SIGTERM, &s_action, NULL) != 0) {
+  struct sigaction saction;
+  memset(&saction, 0, sizeof(saction));
+  saction.sa_handler = signal_handler;
+  if (sigaction(SIGTERM, &saction, NULL) != 0) {
     syslog(LOG_ERR, "Error registering SIGTERM: %m");
   }
-  if (sigaction(SIGINT, &s_action, NULL) != 0) {
+  if (sigaction(SIGINT, &saction, NULL) != 0) {
     syslog(LOG_ERR, "Error registering SIGINT: %m");
   }
 
-  // Create a socket
   socket_fd = socket(PF_INET, SOCK_STREAM, 0);
   if (socket_fd < 0) {
     printf("Socket could not be created: %m\n");
     exit(-1);
   }
-  
-  // Set socket options
   if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &sockflgs,
                  sizeof(sockflgs))) {
     printf("Could not set socket options: %m\n");
     goto fail;
   }
-  else if (getaddrinfo(NULL, USER_PORT, &myinfoin, &myinfo)) {
+  if (getaddrinfo(NULL, "9000", &myaddrinfoin, &myaddrinfo)) {
     printf("gettaddrinfo failed: %m\n");
-    freeaddrinfo(myinfo);
+    freeaddrinfo(myaddrinfo);
     goto fail;
   }
-  else if (bind(socket_fd, myinfo->ai_addr, sizeof(struct sockaddr)) < 0) {
+  if (bind(socket_fd, myaddrinfo->ai_addr, sizeof(struct sockaddr)) < 0) {
     printf("bind failed: %m\n");
     goto fail;
   }
-  freeaddrinfo(myinfo);
-  else if (listen(socket_fd, 5)) {
+  freeaddrinfo(myaddrinfo);
+  if (listen(socket_fd, 5)) {
     printf("listen failed: %m\n");
     goto fail;
   }
 
+  // doing this so late because we want to be sure first that
+  // port 9000 is free
   while ((opt = getopt(argc, argv, "dh")) != -1)
     switch (opt) {
     case 'd':
@@ -106,11 +94,9 @@ int main(int argc, char *argv[]) {
     goto fail;
   }
   while (1) {
-    int buf_size = 1;
-  
     struct sockaddr_in addr;
     socklen_t addr_len = sizeof(addr);
-    int client_fd = accept(socket_fd, (struct sockaddr *)&addr, &addr_len);// Accept connection
+    int client_fd = accept(socket_fd, (struct sockaddr *)&addr, &addr_len);
     if (client_fd < 0) {
       printf("accept failed: %m\n");
       goto fail;
@@ -121,8 +107,7 @@ int main(int argc, char *argv[]) {
 
     char *msgbuf = malloc(sizeof(char));
     *msgbuf = '\0';
-    
-    // Receive data
+    int buf_size = 1;
     while (1) {
       char recv_buf[BUFFER_SIZE + 1];
       int recvd = recv(client_fd, recv_buf, BUFFER_SIZE, 0);
@@ -130,22 +115,19 @@ int main(int argc, char *argv[]) {
         syslog(LOG_ERR, "Error receiving: %m");
         close(client_fd);
         goto fail;
-      } 
-      else if (!recvd) {
+      } else if (!recvd) {
         syslog(LOG_INFO, "Closed connection from %s", client_addr);
         break;
       }
       recv_buf[recvd] = '\0';
       char *str = recv_buf;
       char *token = strsep(&str, "\n");
-      
       if (str == NULL) {
-        // haven't found the \n yet, more data to come
+        // haven't found the \n yet, there is more dta to come
         buf_size += recvd;
         msgbuf = realloc(msgbuf, buf_size * sizeof(char));
         strncat(msgbuf, token, recvd);
-      } 
-      else {
+      } else {
         buf_size += strlen(token);
         msgbuf = realloc(msgbuf, buf_size * sizeof(char));
         strcat(msgbuf, token);
@@ -186,8 +168,7 @@ int main(int argc, char *argv[]) {
 fail1:
   fclose(data_fd);
   remove(DATA_PATH);
-  
 fail:
   close(socket_fd);
-  return -1;
+  exit(-1);
 }
